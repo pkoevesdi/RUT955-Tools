ec123b52434c70f2e2822c2c19dfa1acef459ca2
p.koevesdi <p.koevesdi@enerkite.de>
2020-05-31 22:18:52 +0200

    ADDED: logging into subfolders by topic

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Simple MQTT-Logger for Busybox or other shells. Writes all MQTT messages from su
 
 Call script with same options like for [mosquitto_sub](https://mosquitto.org/man/mosquitto_sub-1.html). '-v' gets added. Configure output directory and other parameters inside script.
 
+:warning: **If using the outputsubdirs flag** (for logging into subfolders by topic) with topics that differ much in their posting frequency, you can easily loose data because of the log rotation. The rarely used topic can easily become the oldest file and if the configured space quota is used up, it gets deleted.
+
 Tested on a router Teltonika RUT955 running BusyBox v1.30.1 () built-in shell (ash)
 
 Based on https://unix.stackexchange.com/a/274224
--- mqtt.sh
+++ mqtt.sh
@@ -2,66 +2,65 @@
 ###################
 # MQTT Shell Logger
 
-outputdir="/mnt/mmcblk0p1"
+outputroot="/mnt/mmcblk0p1" # Scope to put and rotate the logs in, thus oldest file inside this might get deleted, even if not lyed out by this script!
+outputsubdirs=1             # whether to create subdirectories for each topic
 filenameprefix="log_"
 filenamesuffixformat="+%Y-%m-%dT%H_%M_%S%z" # format string for date command
 fileextension="csv"
-#minfreespace=31149644 # in KiB (for testing)
+#minfreespace=31149460 # in KiB (for testing)
 minfreespace=2097 # in KiB
 #maxfilesize=1048 # in B (for testing)
 maxfilesize=1048576 # in B
 
-p="/tmp/backpipe";
-pidfile="/tmp/pidfile"
+backpipe="/tmp/mqttlogger_backpipe"
+pidfile="/tmp/mqttlogger_pidfile"
 
 ctrl_c() {
   echo "Cleaning up..."
-  pid=$(cat $pidfile)
-  rm -f $p
-  rm -f $pidfile  
-  kill $pid 2>/dev/null
-  if [[ "$?" -eq "0" ]];
-  then
-     echo "Exit success";exit 0
-  else
-     exit 1
-  fi
+  kill $(cat $pidfile)  2>/dev/null
+  rm -f $backpipe $pidfile
+  [ "$?" -eq "0" ] && { echo "Exit success"; exit 0; } || exit 1
 }
 
 listen(){
 
-  ([ ! -p "$p" ]) && mkfifo $p
-  (mosquitto_sub -v $*>$p 2>/dev/null) &
+  [ ! -p "$backpipe" ] && mkfifo $backpipe
+  (mosquitto_sub -v $*>$backpipe 2>/dev/null) &
   echo "$!" > $pidfile
 
-  while read line <$p
+  while read line <$backpipe
   do
 
     #timestamp=$(date -Ins)                      # option ns not available on busybox
     ns=$(adjtimex | awk 'NR==12 {print $NF}')    # get nanoseconds on busybox
     timestamp=$(date "+%Y-%m-%dT%H:%M:%S.$ns%z") # workaround for not having +%N or -Ins options in date
     #timestamp=$(date "+%Y-%m-%dT%H:%M:%S%z")    # give up on nanoseconds
-    
+
     topic=${line%%" "*}
     message=${line#*" "}
+    outputpath="$outputroot"$([ $outputsubdirs -ne 0 ] && echo "/$topic")
+
+    # make new directory if necessary
+    # otherwise get last changed logfile inside the subfolder
+    [ ! -d $outputpath ] && { mkdir -p $outputpath; unset filename; } || \
+    filename=$(find $outputpath -maxdepth 1 -type f -name "$filenameprefix*.$fileextension" -print0 | xargs -0r ls -t | head -n1)
 
     # debugging:
-    #echo "topic: "$topic
-    #echo "message: "$message
+#    echo "topic=$topic"
+#    echo "message=$message"
+#    echo "outputpath: $outputpath"
 
-    # new file if nessesary:
-    #filesize=$(wc -c "$filename")
-    #filesize=${filesize%%" "*}
+    # new file if necessary:
     ([ -z "$filename" ] || \
     (filesize=$(wc -c "$filename") && filesize=${filesize%%" "*} && [ "$filesize" -gt "$maxfilesize" ])) && \
-    filename="$outputdir/$filenameprefix$(date $filenamesuffixformat).$fileextension"
-    
-    # delete oldest, if nessesary:
-    avail=$(df "$outputdir" | awk 'NR == 2 { print $4 }') # get free disk space
-    [ "$avail" -lt "$minfreespace" ] && rm "$(ls $outputdir/log_* -ltr | grep -v '^d' | awk 'NR==1 {print $NF; exit}')"
+    filename="$outputpath/$filenameprefix$(date $filenamesuffixformat).$fileextension"
+
+    # delete oldest, if necessary:
+    avail=$(df "$outputroot" | awk 'NR == 2 { print $4 }') # get free disk space
+    [ "$avail" -lt "$minfreespace" ] && rm "$(find $outputroot -type f -name "$filenameprefix*.$fileextension" -print0 | xargs -0r ls -tr | head -n1)"
 
     # write log entry:
-    echo "\"$timestamp\",\"$topic\",\"$message\"">>"$filename"
+    echo "\"$timestamp\"$([ $outputsubdirs -eq 0 ] && echo ",\"$topic\""),\"$message\"">>"$filename"
 
   done
 }
@@ -77,12 +76,12 @@ usage(){
 
 case "$1" in
 --h)
-usage                                                           
-exit 1         
-;;             
+usage
+exit 1
+;;
 *)
-trap ctrl_c INT                                                 
-listen $* 
-;;                                                              
+trap ctrl_c INT
+listen $*
+;;
 esac
 
