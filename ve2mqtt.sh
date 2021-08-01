480ee01980385a983f33547950a69a1e1fc28e46
p.koevesdi <p.koevesdi@enerkite.de>
2021-08-01 17:26:23 +0200

    added syslogger, added waiting for mqtt broker

--- ve2mqtt.sh
+++ ve2mqtt.sh
@@ -1,28 +1,46 @@
 #!/bin/sh
 ############
-# GPS 2 MQTT
+# VE 2 MQTT
+
+temp_dir=$(mktemp -d)
+
+mkfifo "$temp_dir/out" "$temp_dir/err"
+<"$temp_dir/out" logger -p user.notice -t "$(basename "$0")" &
+<"$temp_dir/err" logger -p user.error -t "$(basename "$0")" &
+exec >"$temp_dir/out" 2>"$temp_dir/err"
 
 fh="/dev/ttyUSB0"
 stty -F $fh 19200 raw -echo -echoe -echok
 fields=" PID FW SER V I VPV PPV CS MPPT OR ERR LOAD H19 H20 H21 H22 H23 HSDS "
-backpipe="/tmp/mqttve_backpipe"
-pidfile="/tmp/mqttve_pidfile"
+backpipe="$temp_dir/mqttve_backpipe"
+pidfile="$temp_dir/mqttve_pidfile"
 
 ctrl_c() {
-  echo "Cleaning up..."
-  kill $(cat $pidfile) 2>/dev/null
-  rm -f $backpipe $pidfile
+  kill $(cat "$pidfile")
+  rm -rf $temp_dir
   [ "$?" -eq "0" ] && {
-    echo "Exit success"
+    echo "Exit success."
     exit 0
   } || exit 1
 }
 
+
 listen() {
 
+  while true
+  do
+     curl -s mqtt://localhost:1883
+     ret=$?
+     [ $ret -eq 3 ] && unset tmp && unset ret && break
+     [ -z $tmp ] && echo "waiting for mqtt broker" && tmp=1
+     sleep 1
+  done
+
   [ ! -p "$backpipe" ] && mkfifo $backpipe
-  (cat $fh $* >$backpipe 2>/dev/null) &
-  echo "$!" >$pidfile
+  (cat $fh $* >$backpipe) &
+  echo "$!" >"$pidfile"
+
+  echo "started."
 
   while read name value; do
 
@@ -38,9 +56,9 @@ listen() {
         eval $name="'$value'"
 		if [ "$name" == "LOAD" ]; then
 			if [ "$value" == "ON" ]; then
-				gpio.sh set DOUT2
+				ubus call ioman.relay.relay0 update '{"state":"closed"}'
 			elif [ "$value" == "OFF" ]; then
-				gpio.sh clear DOUT2
+				ubus call ioman.relay.relay0 update '{"state":"open"}'
 			fi
 		fi
         #    else
@@ -67,7 +85,7 @@ case "$1" in
   exit 1
   ;;
 *)
-  trap ctrl_c INT
+  trap ctrl_c INT TERM QUIT
   listen $*
   ;;
 esac
